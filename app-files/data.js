88a91a7a3e9b0a44b4f6caebd99a1addf4d43874
cefoot <chris.papenfuss@data-experts.de>
2018-10-05 08:48:52 +0200

    generated 360 image

---
+++ app-files/data.js
@@ -0,0 +1,73 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-yivr_stitch_0846_360",
+      "name": "YIVR_Stitch_0846_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.4251604156279214,
+          "pitch": -0.2726366795504056,
+          "title": "HoloLens with GoPro",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.8142055380694853,
+          "pitch": -0.5596406214748164,
+          "title": "\"User\"-HoloLens (multiple possible)",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.7447813516320885,
+          "pitch": -0.39007841007074084,
+          "title": "live view",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.6117545212918287,
+          "pitch": 0.2117371607470453,
+          "title": "PC to capture GoPro Image and render 3d content",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.3036191121092724,
+          "pitch": -0.3659410447377738,
+          "title": "Visitors without HoloLens",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Spectator view setup",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
